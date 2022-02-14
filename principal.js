bcca637881dd4cab49c9623336b2693532382dff
Lucas_Bunny <lucas08ms@gmail.com>
2022-02-14 08:12:49 -0300

    Correção do arquivo javascript

--- principal.js
+++ principal.js
@@ -1,5 +1,4 @@
-var banners = ["Os melhores móveis de São Paulo!!", "Qualidade e preço 
-muitoooo baixo!"];
+var banners = ["Os melhores móveis de São Paulo!!", "Qualidade e preço muitoooo baixo!"];
 var bannerAtual = 0;
 function trocarBanner() {
 bannerAtual = (bannerAtual + 1) % 2;
